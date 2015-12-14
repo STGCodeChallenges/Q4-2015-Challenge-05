71184c851e2e8b57633e4b749770ae195fd97fbe
quinn2002 <quinnyboy02@hotmail.com>
2015-12-14 13:23:41 -0700

    Added project description

--- QuinnHeiner/Program.cs
+++ QuinnHeiner/Program.cs
@@ -1,6 +1,23 @@
 ﻿using System;
 using System.Linq;
 
+/* Challenge #5 (due 12/21/2015): Star Wars Trivia
+With all the hype around Star Wars: The Force Awakens, we’ve been tasked to come up with a Star Wars trivia game in order to refresh everyone’s memories of the previous episodes.
+
+Description
+The sky's the limit on what kind of UI you want to create--it can be as ugly or fancy as you’d like it.  These are the requirements:
+The ability for the user to choose between at least 2 levels of difficulty
+At least five randomly selected questions from a larger bank of questions (within the selected level of difficulty).  
+Only one answer per question should be considered correct.
+At the end of the quiz, print out the results, including the total score, along with a breakdown of each question, the user’s response, and the correct response.
+Feel free to copy whatever questions/answers you’d like from the internet (or make up your own).  A suggested minimum is around 10 questions per difficulty to ensure a decent sampling of questions.
+
+Bonus Entry
+Extend the game to allow multiple people/teams to submit answers to each question, rather than just a single person.  
+Of course, most UIs will allow players to see each others’ responses, but to keep things simple, that’s okay.  
+Same rules above apply, except for the results will be broken down by each person/team, with a winner determined (or winners in the case of a tie).
+ */
+
 namespace CodeChallenge05_StarWarsTrivia
 {
 	public class Program
