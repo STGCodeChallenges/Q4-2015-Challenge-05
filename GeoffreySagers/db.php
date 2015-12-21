1757afe24c8e3b7a1de514b7a2edce4fb83518ef
Geoffrey Sagers <geoffrey@sagersweb.com>
2015-12-21 16:51:34 -0700

    Added files for Geoffrey Sagers

---
+++ GeoffreySagers/db.php
@@ -0,0 +1,189 @@
+<?php
+$title = "Star Wars Trivia";
+
+$easy = array(
+            array(
+            "question"=>"Who is Darth Vader",
+            "answer"=> array(
+                0=>"Father of Luke Skywalker",
+                1=>"Mother of Luke Skywalker",
+                2=>"Sister of Luke Skywalker",
+                3=>"Brother of Luke Skywalker"
+            )
+        ),
+            array(
+            "question"=>"Who is Princess Leia",
+            "answer"=> array(
+                0=>"Sister of Luke Skywalker",
+                1=>"Mother of Luke Skywalker",
+                2=>"Father of Luke Skywalker",
+                3=>"Brother of Luke Skywalker"
+                )
+        ),
+            array(
+            "question"=>"What color is Darth Vader's light saber",
+            "answer"=> array(
+                0=>"Red",
+                1=>"Blue",
+                2=>"Green",
+                3=>"Purple"
+                )
+        ),
+            array(
+            "question"=>"What color is C-3P0",
+            "answer"=> array(
+                0=>"Gold",
+                1=>"Silver",
+                2=>"Blue",
+                3=>"Pink"
+                )
+        ),
+            array(
+            "question"=>"Who is the pilot/owner of the Millenium Falcon",
+            "answer"=> array(
+                0=>"Han Solo",
+                1=>"Bobba Fett",
+                2=>"Jabba the Hutt",
+                3=>"George Lucas"
+                )
+        ),
+            array(
+            "question"=>"Which of the following is a bounty hunter",
+            "answer"=> array(
+                0=>"Bobba Fett",
+                1=>"R2-D2",
+                2=>"Yoda",
+                3=>"Luke Skywalker"
+                )
+        ),
+            array(
+            "question"=>"What color is Yoda's skin",
+            "answer"=> array(
+                0=>"Green",
+                1=>"Yellow",
+                2=>"Orange",
+                3=>"Blue"
+                )
+        ),
+            array(
+            "question"=>"What creatures live on Endor",
+            "answer"=> array(
+                0=>"Ewoks",
+                1=>"Tribbles",
+                2=>"Care Bears",
+                3=>"Lycans"
+                )
+        ),
+            array(
+            "question"=>"What is Luke Skywalker training to become",
+            "answer"=> array(
+                0=>"Jedi",
+                1=>"Starfleet captain",
+                2=>"Chief medical officer",
+                3=>"Sith lord"
+                )
+        ),
+            array(
+            "question"=>"Who kills Greedo in the Mos Eisley Cantina",
+            "answer"=> array(
+                0=>"Han Solo",
+                1=>"Princess Leia",
+                2=>"2 Ewoks and a Wookie",
+                3=>"Chewbacca"
+                )
+        )
+ );
+
+$hard = array(
+            array(
+            "question"=>"Which of the following is NOT a type of Rebel Fighter",
+            "answer"=> array(
+                0=>"M-Wing",
+                1=>"X-Wing",
+                2=>"Y-Wing",
+                3=>"B-Wing"
+            )
+        ),
+            array(
+            "question"=>"What is Yoda's last name",
+            "answer"=> array(
+                0=>"He does not have one",
+                1=>"Degobah",
+                2=>"Swampwater",
+                3=>"Nan-uke"
+                )
+        ),
+            array(
+            "question"=>"What race/species is Admiral Akbar a member of",
+            "answer"=> array(
+                0=>"Mon Calamari",
+                1=>"Ugnaught",
+                2=>"Caamasi",
+                3=>"Myneyrsh"
+                )
+        ),
+            array(
+            "question"=>"What planet is Chewbacca from",
+            "answer"=> array(
+                0=>"Kashyyyk",
+                1=>"Endor",
+                2=>"Tatooine",
+                3=>"Hoth"
+                )
+        ),
+            array(
+            "question"=>"Where is the Cantina at on Tatooine",
+            "answer"=> array(
+                0=>"Mos Eisley",
+                1=>"Coruscant",
+                2=>"Naboo",
+                3=>"Bespin"
+                )
+        ),
+            array(
+            "question"=>"Which of the following is a NOT a bounty hunter",
+            "answer"=> array(
+                0=>"5-LOM",
+                1=>"IGN-88",
+                2=>"Bossk",
+                3=>"Bobba Fett"
+                )
+        ),
+            array(
+            "question"=>"From which planet is Han Solo",
+            "answer"=> array(
+                0=>"Corellia",
+                1=>"Tatooine",
+                2=>"Kashyyyk",
+                3=>"Hoth"
+                )
+        ),
+            array(
+            "question"=>"Darth Tyranus is also known by which name",
+            "answer"=> array(
+                0=>"Count Dooku",
+                1=>"Emperor Palpatine",
+                2=>"Lord Sauramaun",
+                3=>"Baron Dathomir"
+                )
+        ),
+            array(
+            "question"=>"Whose DNA were the clones made from",
+            "answer"=> array(
+                0=>"Jango Fett",
+                1=>"Bobba Fett",
+                2=>"Geonosians",
+                3=>"Sith lord"
+                )
+        ),
+            array(
+            "question"=>"Who is Chewbacca's Dad",
+            "answer"=> array(
+                0=>"Attichitcuk",
+                1=>"Wicket",
+                2=>"Mallatobuck",
+                3=>"Shyriiwook"
+                )
+        )
+ );
+?>
